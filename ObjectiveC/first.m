7ce090b61b1cc42cfe327e452f4ac04e87b596f1
Amit Snyderman <amit@amitsnyderman.com>
2009-01-29 22:17:03 -0500

    Finalized Class #2 assignment

--- ObjectiveC/first.m
+++ ObjectiveC/first.m
@@ -1,44 +1,5 @@
 #import <Foundation/Foundation.h>
 
-#pragma mark MyClass
-
-@interface MyClass : NSObject {
-	float myFloat;
-}
-
-- (void)hello;
-
-@end
-
-@implementation MyClass
-
-- (id)init {
-	self = [super init];
-	if (self != nil) {
-		myFloat = 5.0;
-	}
-	return self;
-}
-
-- (void)hello {
-	NSLog(@"Hello, I'm your first Objective-C program!");
-}
-
-- (void)setMyFloat:(float)val {
-	myFloat = val;
-}
-
-- (float)myFloat {
-	return myFloat;
-}
-
-- (void)dealloc {
-	[super dealloc];
-}
-
-@end
-
-#pragma mark -
 #pragma mark YourClass
 
 @interface YourClass : NSObject {}
@@ -65,6 +26,60 @@
 @end
 
 #pragma mark -
+#pragma mark MyClass
+
+@interface MyClass : NSObject {
+	float myFloat;
+	YourClass *friend;
+}
+
+- (void)hello;
+- (float)myFloat;
+- (void)setMyFloat:(float)val;
+- (YourClass*)friend;
+- (void)setFriend:(YourClass*)aFriend;
+
+@end
+
+@implementation MyClass
+
+- (id)init {
+	self = [super init];
+	if (self != nil) {
+		myFloat = 5.0;
+	}
+	return self;
+}
+
+- (void)hello {
+	NSLog(@"Hello, I'm your first Objective-C program!");
+}
+
+- (float)myFloat {
+	return myFloat;
+}
+
+- (void)setMyFloat:(float)val {
+	myFloat = val;
+}
+
+- (YourClass*)friend {
+	return friend;
+}
+
+- (void)setFriend:(YourClass*)aFriend {
+	[friend release];
+	friend = [aFriend retain];
+}
+
+- (void)dealloc {
+	[friend dealloc];
+	[super dealloc];
+}
+
+@end
+
+#pragma mark -
 #pragma mark Runtime
 
 int main(int argc, char **argv) {
@@ -72,10 +87,15 @@ int main(int argc, char **argv) {
 	
 	MyClass *instance = [[MyClass alloc] init];
 	[instance hello];
-	
 	[instance setMyFloat:10.0f];
 	NSLog(@"New value is %f", [instance myFloat]);
 	
+	YourClass *mine = [[YourClass alloc] init];
+	[instance setFriend:mine];
+	NSLog(@"This is my friend %@ and he says '%@'", [instance friend], [[instance friend] goodbye]);
+	
+	[mine release];
+	[instance release];
 	[pool release];
 	
 	return 0;
